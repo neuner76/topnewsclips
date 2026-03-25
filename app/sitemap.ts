c7751ce85fd271c07436441e648cb762f4c8cd8f
neuner76 <neuner@gmail.com>
2026-03-24 17:28:35 -0700

    fix: sitemap www URL and add revalidation

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from 'next'
 import { createClient } from '@supabase/supabase-js'
 
+export const revalidate = 300
+
 const SITE_URL = 'https://www.topnewsclips.com'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
